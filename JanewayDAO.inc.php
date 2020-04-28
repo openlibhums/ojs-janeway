745ab200805d830c71d0214bd229583ce9a5e3fb
Andy Byers <andy.byers@openlibhums.org>
2020-04-28 15:36:20 +0100

    Added /users/ view that lists a journal's users

--- JanewayDAO.inc.php
+++ JanewayDAO.inc.php
@@ -6,9 +6,15 @@
  * Written by Andy Byers, Birkbeck COllege
  *
  */
+import('classes.user.User');
+import('classes.user.UserDAO');
 
-
-class JanewayDAO extends DAO {
-
+class JanewayDAO extends UserDAO {
+	function &getAllUsers($journalId) {
+		$sql = 'SELECT DISTINCT u.* FROM users u LEFT JOIN roles r ON u.user_id=r.user_id WHERE (r.journal_id='.$journalId.') ';
+		$result =& $this->retrieveRange($sql);
+		$returner = new DAOResultFactory($result, $this, '_returnUserFromRowWithData');
+		return $result;
+	}
 }
 
